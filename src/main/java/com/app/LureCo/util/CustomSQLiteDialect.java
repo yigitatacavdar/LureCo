89273f9d8582a67aaf37be49bfcc45457363a5fa
yigit <atacavdar7@gmail.com>
2025-12-03 17:32:08 +0300

    fixed id incrementing issue on custom sqlite dialect

--- src/main/java/com/app/LureCo/util/CustomSQLiteDialect.java
+++ src/main/java/com/app/LureCo/util/CustomSQLiteDialect.java
@@ -1,8 +1,8 @@
 package com.app.LureCo.util;
 
 import org.hibernate.community.dialect.SQLiteDialect;
-
 import org.hibernate.dialect.identity.IdentityColumnSupport;
+import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
 
 public class CustomSQLiteDialect extends SQLiteDialect {
 
@@ -13,11 +13,21 @@ public class CustomSQLiteDialect extends SQLiteDialect {
         return IDENTITY_SUPPORT;
     }
 
+    public static class SQLiteAutoIncrementIdentityColumnSupport extends IdentityColumnSupportImpl {
 
-    public static class SQLiteAutoIncrementIdentityColumnSupport extends org.hibernate.community.dialect.identity.SQLiteIdentityColumnSupport {
+        @Override
+        public boolean supportsIdentityColumns() {
+            return true;
+        }
+
         @Override
         public String getIdentityColumnString(int type) {
             return "integer primary key autoincrement";
+        }
+
+        @Override
+        public String getIdentitySelectString(String table, String column, int type) {
+            return "select last_insert_rowid()";
         }
     }
 }
