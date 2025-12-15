c97fe29b6d73ea2eb9174d95bdb65dfbeb3979f9
korki many <nothinkingjustoverthinking@gmail.com>
2025-12-15 14:58:18 +0300

    add index.html font problem solved
    add scrollbar slide template done
    
    add datatable and forms reversed

--- src/main/webapp/resources/fish/main__2.js
+++ src/main/webapp/resources/fish/main__2.js
@@ -3,3 +3,9 @@ fetch("/LureCo/resources/fish/l.svg")
   .then(svg => {
     document.getElementById("logo").innerHTML = svg;
   });
+  
+  fetch("/LureCo/resources/fish/fish-background.svg")
+    .then(res => res.text())
+    .then(svg => {
+      document.getElementById("logo--2").innerHTML = svg;
+    });
